ace92587c964adc8f0a358674b3a0de381178770
Changsu Park <gd12091@gmail.com>
2025-04-28 16:38:38 +0900

    ✨ feat: Terrafrom Backend 구현
    - S3를 이용한 tfstate 관리
    - DynamoDB를 이용한 상태 Lock 구현

---
+++ terraform/v1/envs/mvp/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# 추후 모듈 개발시, versions.tf로 해당 내용 이관
