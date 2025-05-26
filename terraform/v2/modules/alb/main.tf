9cb78a5982c29ff0d54b4b13aeb14188c0171e72
yubin425 <yubin6gkrsus@gmail.com>
2025-05-26 14:23:33 +0900

    ♻️ refactor : Refactor alb listener

--- terraform/v2/modules/alb/main.tf
+++ terraform/v2/modules/alb/main.tf
@@ -63,7 +63,7 @@ resource "aws_lb_target_group" "back_green" {
 
 resource "aws_lb_target_group" "front_blue" {
   name     = "${var.name_prefix}-${var.common_tags.Environment}-tg-front-blue"
-  port     = var.back_target_group_port
+  port     = var.front_target_group_port
   protocol = "HTTP"
   vpc_id   = var.target_group_vpc_id
 
@@ -150,27 +150,9 @@ resource "aws_lb_listener" "fe_prod" {
     }
   )
 }
-
-resource "aws_lb_listener" "fe_test" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = 1443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.front_green.arn
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-${var.common_tags.Environment}-fe-test-listener"
-  }
-}
-
 resource "aws_lb_listener" "be_prod" {
   load_balancer_arn = aws_lb.this.arn
-  port              = 9443
+  port              = 8080
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.certificate_arn
@@ -184,24 +166,6 @@ resource "aws_lb_listener" "be_prod" {
     Name = "${var.name_prefix}-${var.common_tags.Environment}-be-prod-listener"
   }
 }
-
-resource "aws_lb_listener" "be_test" {
-  load_balancer_arn = aws_lb.this.arn
-  port              = 19443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.back_green.arn
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-${var.common_tags.Environment}-be-test-listener"
-  }
-}
-
 
 
 
