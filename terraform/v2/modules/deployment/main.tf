2f757ead4b88a2c42385443ef8cacdc1ac64829d
yubin425 <yubin6gkrsus@gmail.com>
2025-06-10 11:18:12 +0900

    ♻️ refactor : s3 주석 제거, autoscailng groups 블록을 변수로 수정
    - s3가 리소스로 있지 않으면 output을 통한 iam 생성이 불가능해서 다시 생성함

--- terraform/v2/modules/deployment/main.tf
+++ terraform/v2/modules/deployment/main.tf
@@ -1,13 +1,13 @@
-# resource "aws_s3_bucket" "codedeploy_bucket" {
-#   bucket = "${var.name_prefix}-${var.app_name}-codedeploy-bucket"
+resource "aws_s3_bucket" "codedeploy_bucket" {
+  bucket = "${var.name_prefix}-${var.app_name}-codedeploy-bucket"
 
-#   tags = merge(
-#     local.default_tags,
-#     {
-#       Name = "${var.name_prefix}-${var.app_name}-codedeploy-bucket"
-#     }
-#   )
-# }
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "${var.name_prefix}-${var.app_name}-codedeploy-bucket"
+    }
+  )
+}
 
 resource "aws_codedeploy_app" "this" {
   name             = "${var.name_prefix}-${var.app_name}-${var.common_tags.Environment}-deployment"
@@ -27,45 +27,41 @@ resource "aws_codedeploy_deployment_group" "this" {
   service_role_arn       = var.service_role_arn
   deployment_config_name = var.deployment_config_name
 
-  dynamic "autoscaling_groups" {
-    for_each = var.blue_green ? [1] : []
-    content {
-      name = var.auto_scaling_groups
-    }
-  }
+  autoscaling_groups = var.auto_scaling_groups
+
 
   dynamic "deployment_style" {
     for_each = var.blue_green ? [1] : []
     content {
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-    deployment_type   = "BLUE_GREEN"
-  }
-}
-
-dynamic "blue_green_deployment_config" {
-  for_each = var.blue_green ? [1] : []
-  content{
-    green_fleet_provisioning_option {
-      action = "COPY_AUTO_SCALING_GROUP"
-    }
-
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-    }
-    terminate_blue_instances_on_deployment_success {
-      action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 60
-    }
-   }
-  } 
-
- dynamic "load_balancer_info" {
-  for_each = var.blue_green ? [1] : []
-  content {
-    target_group_info {
-      name = var.target_group_blue
+      deployment_option = "WITH_TRAFFIC_CONTROL"
+      deployment_type   = "BLUE_GREEN"
     }
   }
+
+  dynamic "blue_green_deployment_config" {
+    for_each = var.blue_green ? [1] : []
+    content {
+      green_fleet_provisioning_option {
+        action = "COPY_AUTO_SCALING_GROUP"
+      }
+
+      deployment_ready_option {
+        action_on_timeout = "CONTINUE_DEPLOYMENT"
+      }
+      terminate_blue_instances_on_deployment_success {
+        action                           = "TERMINATE"
+        termination_wait_time_in_minutes = 60
+      }
+    }
+  }
+
+  dynamic "load_balancer_info" {
+    for_each = var.blue_green ? [1] : []
+    content {
+      target_group_info {
+        name = var.target_group_blue
+      }
+    }
 
   }
 
