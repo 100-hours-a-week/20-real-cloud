d79bc02dd8801e1ea087b555ed3015f57e163ebd
shinnyriver <gd12091@gmail.com>
2025-06-14 23:30:37 +0900

    🐛 fix: ssm region

--- terraform/v2/modules/compute/scripts/db_userdata.sh
+++ terraform/v2/modules/compute/scripts/db_userdata.sh
@@ -14,9 +14,9 @@ systemctl enable redis-server
 systemctl start redis-server
 
 # 3. 비밀번호 SSM에서 가져오기
-MYSQL_ROOT_PASSWORD=$(aws ssm get-parameter --name "/config/prod/db_passwd" --with-decryption --query "Parameter.Value" --output text)
-DB_USERNAME=$(aws ssm get-parameter --name "/config/prod/db_user" --with-decryption --query "Parameter.Value" --output text)
-DB_PASSWORD=$(aws ssm get-parameter --name "/config/prod/db_passwd" --with-decryption --query "Parameter.Value" --output text)
+MYSQL_ROOT_PASSWORD=$(aws ssm get-parameter --region ap-northeast-2 --name "/config/prod/db_passwd" --with-decryption --query "Parameter.Value" --output text)
+DB_USERNAME=$(aws ssm get-parameter --region ap-northeast-2 --name "/config/prod/db_user" --with-decryption --query "Parameter.Value" --output text)
+DB_PASSWORD=$(aws ssm get-parameter --region ap-northeast-2 --name "/config/prod/db_passwd" --with-decryption --query "Parameter.Value" --output text)
 
 # 4. MySQL 루트 비밀번호 설정
 mysql -u root <<EOF
@@ -35,8 +35,8 @@ EOF
 S3_BUCKET="s3://ktb-ca-mvp-log/mysql-backups"
 TMP_DIR="/tmp/mysql_restore"
 mkdir -p $TMP_DIR
-LATEST_DUMP=$(aws s3 ls $S3_BUCKET/ | sort | tail -n 1 | awk '{print $4}')
-aws s3 cp "$S3_BUCKET/$LATEST_DUMP" "$TMP_DIR/latest_dump.sql.gz"
+LATEST_DUMP=$(aws s3 ls --region ap-northeast-2 $S3_BUCKET/ | sort | tail -n 1 | awk '{print $4}')
+aws s3 cp --region ap-northeast-2 "$S3_BUCKET/$LATEST_DUMP" "$TMP_DIR/latest_dump.sql.gz"
 gunzip -c "$TMP_DIR/latest_dump.sql.gz" | mysql -u root -p"$MYSQL_ROOT_PASSWORD"
 
 # 7. 백업 및 로그 업로드용 크론잡 스크립트 생성
@@ -50,11 +50,11 @@ DUMP_FILE="/tmp/mysql_backup_$DATE.sql"
 GZ_FILE="${DUMP_FILE}.gz"
 BUCKET="s3://ktb-ca-mvp-log/mysql-backups"
 
-MYSQL_ROOT_PASSWORD=$(aws ssm get-parameter --name "/config/prod/db_passwd" --with-decryption --query "Parameter.Value" --output text)
+MYSQL_ROOT_PASSWORD=$(aws ssm get-parameter --region ap-northeast-2 --name "/config/prod/db_passwd" --with-decryption --query "Parameter.Value" --output text)
 
 mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" --all-databases > "$DUMP_FILE"
 gzip "$DUMP_FILE"
-aws s3 cp "$GZ_FILE" "$BUCKET/"
+aws s3 cp --region ap-northeast-2 "$GZ_FILE" "$BUCKET/"
 rm -f "$GZ_FILE"
 EOS
 chmod +x $CRON_SCRIPT
